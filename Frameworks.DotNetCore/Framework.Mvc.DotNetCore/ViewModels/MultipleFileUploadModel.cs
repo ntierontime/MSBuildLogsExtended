410db2d77e1fd24b2ab0a7f9301e739402c2a55d
David Zhao <ntierontime@gmail.com>
2016-11-10 09:45:44 -0500

    removed/commented out some files

--- Frameworks.DotNetCore/Framework.Mvc.DotNetCore/ViewModels/MultipleFileUploadModel.cs
+++ Frameworks.DotNetCore/Framework.Mvc.DotNetCore/ViewModels/MultipleFileUploadModel.cs
@@ -1,22 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
 
-namespace Framework.Mvc
-{
-    public class MultipleFileUploadModel<TMasterEntity, TCriteriaOfMasterEntity, TStatusOfLoadMasterEntity> : Framework.Mvc.ViewModelEntityRelatedBase<TMasterEntity, TCriteriaOfMasterEntity, TStatusOfLoadMasterEntity>
-        where TMasterEntity : class, new()
-        where TCriteriaOfMasterEntity : class, new()
-    {
-        public MultipleFileUploadModel(TCriteriaOfMasterEntity criteriaOfMasterEntity)
-            : base(criteriaOfMasterEntity)
-        {
-        }
-        public string FilesToBeUploaded { get; set; }
-        public List<HttpPostedFileBase> FileUploads { get; set; }
+//namespace Framework.Mvc
+//{
+//    public class MultipleFileUploadModel<TMasterEntity, TCriteriaOfMasterEntity, TStatusOfLoadMasterEntity> : Framework.Mvc.ViewModelEntityRelatedBase<TMasterEntity, TCriteriaOfMasterEntity, TStatusOfLoadMasterEntity>
+//        where TMasterEntity : class, new()
+//        where TCriteriaOfMasterEntity : class, new()
+//    {
+//        public MultipleFileUploadModel(TCriteriaOfMasterEntity criteriaOfMasterEntity)
+//            : base(criteriaOfMasterEntity)
+//        {
+//        }
+//        public string FilesToBeUploaded { get; set; }
+//        public List<HttpPostedFileBase> FileUploads { get; set; }
 		
-    }
+//    }
 
-}
+//}
 
