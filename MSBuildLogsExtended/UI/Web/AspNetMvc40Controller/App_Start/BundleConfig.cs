1a69300065e4d52aa1bcfd1c687c55a79d81f1fe
David Zhao <ntierontime@gmail.com>
2016-12-14 14:31:34 -0500

    started bootstrap, added awesome-bootstrap-checkbox
    awesome-bootstrap-checkbox was modified based on nuget version 0.3.4.1,
    and changed to glyphicons-halflings icons/font from fontawesome.

--- MSBuildLogsExtended/UI/Web/AspNetMvc40Controller/App_Start/BundleConfig.cs
+++ MSBuildLogsExtended/UI/Web/AspNetMvc40Controller/App_Start/BundleConfig.cs
@@ -43,11 +43,12 @@ namespace MSBuildLogsExtended.AspNetMvc40Controller
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css"
                 , "~/Content/bootstrap-datepicker.css"
+                , "~/Content/awesome-bootstrap-checkbox.mvcmodified.css"
                 , "~/Content/bootstrap-theme.css"
                 , "~/Content/DataTables/css/jquery.dataTables.css"
                 , "~/Content/site.css"
                 ));
-
+            
             //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
             //            "~/Scripts/jquery-{version}.js"));
 
@@ -68,29 +69,29 @@ namespace MSBuildLogsExtended.AspNetMvc40Controller
             //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
             //            "~/Scripts/modernizr-*"));
 
-    //        bundles.Add(new StyleBundle("~/Content/css").Include(
-    //            "~/Content/site.css"
-    //            , "~/Content/DataTables-1.9.4/media/css/jquery.dataTables.css"
-    //            , "~/Content/bootstrap.css"
-    //            , "~/Content/bootstrap-datepicker.css"
-    //            , "~/Content/bootstrap-responsive.css"
-    //            , "~/Content/bootstrap-theme.css"
-				//, "~/Content/galleria.classic.css"
-				//));
+            //        bundles.Add(new StyleBundle("~/Content/css").Include(
+            //            "~/Content/site.css"
+            //            , "~/Content/DataTables-1.9.4/media/css/jquery.dataTables.css"
+            //            , "~/Content/bootstrap.css"
+            //            , "~/Content/bootstrap-datepicker.css"
+            //            , "~/Content/bootstrap-responsive.css"
+            //            , "~/Content/bootstrap-theme.css"
+            //, "~/Content/galleria.classic.css"
+            //));
 
-    //        bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-    //                    "~/Content/themes/base/jquery.ui.core.css",
-    //                    "~/Content/themes/base/jquery.ui.resizable.css",
-    //                    "~/Content/themes/base/jquery.ui.selectable.css",
-    //                    "~/Content/themes/base/jquery.ui.accordion.css",
-    //                    "~/Content/themes/base/jquery.ui.autocomplete.css",
-    //                    "~/Content/themes/base/jquery.ui.button.css",
-    //                    "~/Content/themes/base/jquery.ui.dialog.css",
-    //                    "~/Content/themes/base/jquery.ui.slider.css",
-    //                    "~/Content/themes/base/jquery.ui.tabs.css",
-    //                    "~/Content/themes/base/jquery.ui.datepicker.css",
-    //                    "~/Content/themes/base/jquery.ui.progressbar.css",
-    //                    "~/Content/themes/base/jquery.ui.theme.css"));
+            //        bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+            //                    "~/Content/themes/base/jquery.ui.core.css",
+            //                    "~/Content/themes/base/jquery.ui.resizable.css",
+            //                    "~/Content/themes/base/jquery.ui.selectable.css",
+            //                    "~/Content/themes/base/jquery.ui.accordion.css",
+            //                    "~/Content/themes/base/jquery.ui.autocomplete.css",
+            //                    "~/Content/themes/base/jquery.ui.button.css",
+            //                    "~/Content/themes/base/jquery.ui.dialog.css",
+            //                    "~/Content/themes/base/jquery.ui.slider.css",
+            //                    "~/Content/themes/base/jquery.ui.tabs.css",
+            //                    "~/Content/themes/base/jquery.ui.datepicker.css",
+            //                    "~/Content/themes/base/jquery.ui.progressbar.css",
+            //                    "~/Content/themes/base/jquery.ui.theme.css"));
 
             //// knockoutjs
             //bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
