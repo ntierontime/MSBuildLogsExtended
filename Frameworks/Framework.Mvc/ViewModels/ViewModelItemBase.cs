6940c38de3ea629dea7786ec8ece921f4568159a
davidzhao <david4chao@gmail.com>
2017-01-02 12:27:01 -0500

    moved generic view modells to new framework.viewmodels project.

--- Frameworks/Framework.Mvc/ViewModels/ViewModelItemBase.cs
+++ Frameworks/Framework.Mvc/ViewModels/ViewModelItemBase.cs
@@ -1,25 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Framework.Mvc
-{
-    public class ViewModelItemBase<TSearchCriteria, TItem> : Framework.EntityContracts.ViewModelItemBase<TSearchCriteria, TItem>
-        where TSearchCriteria : class, new()
-        where TItem : class, new()
-    {
-        public ViewModelItemBase()
-            : base()
-        {
-            this.StatusOfResult = Framework.CommonBLLEntities.BusinessLogicLayerResponseStatus.UIProcessReady;
-            this.UIActionStatusMessage = new Framework.UIActionStatusMessage();
-        }
-        public Framework.CommonBLLEntities.BusinessLogicLayerResponseStatus StatusOfResult { get; set; }
-        public string StatusMessageOfResult { get; set; }
+//namespace Framework.Mvc
+//{
+//    public class ViewModelItemBase<TSearchCriteria, TItem> : Framework.EntityContracts.ViewModelItemBase<TSearchCriteria, TItem>
+//        where TSearchCriteria : class, new()
+//        where TItem : class, new()
+//    {
+//        public ViewModelItemBase()
+//            : base()
+//        {
+//            this.StatusOfResult = Framework.CommonBLLEntities.BusinessLogicLayerResponseStatus.UIProcessReady;
+//            this.UIActionStatusMessage = new Framework.UIActionStatusMessage();
+//        }
 
-        public Framework.UIActionStatusMessage UIActionStatusMessage { get; set; }
-    }
-}
+//    }
+//}
 
--- Frameworks/Framework.ViewModels/ViewModelItemBase.cs
+++ Frameworks/Framework.ViewModels/ViewModelItemBase.cs
@@ -14,13 +14,23 @@ namespace Framework.EntityContracts
         {
             this.SearchStatus = Framework.EntityContracts.SearchStatus.Unknown;
             this.ContentData = new Framework.EntityContracts.ContentData();
+            this.StatusOfResult = Framework.CommonBLLEntities.BusinessLogicLayerResponseStatus.UIProcessReady;
+            this.UIActionStatusMessage = new Framework.UIActionStatusMessage();
         }
 
         public Framework.EntityContracts.ContentData ContentData { get; set; }
 
         public TSearchCriteria Criteria { get; set; }
+
         public TItem Item { get; set; }
+
         public Framework.EntityContracts.SearchStatus SearchStatus { get; set; }
+
+        public Framework.CommonBLLEntities.BusinessLogicLayerResponseStatus StatusOfResult { get; set; }
+
+        public string StatusMessageOfResult { get; set; }
+
+        public Framework.UIActionStatusMessage UIActionStatusMessage { get; set; }
     }
 }
 
