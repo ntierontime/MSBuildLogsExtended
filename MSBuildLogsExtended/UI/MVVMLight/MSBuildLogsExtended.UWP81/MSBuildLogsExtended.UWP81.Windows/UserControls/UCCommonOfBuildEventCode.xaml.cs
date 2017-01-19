b70a50a3fb0b54ebd7d235232549c5a2515c5f71
David Zhao <ntierontime@gmail.com>
2017-01-19 16:45:32 -0500

    ready for uwp

---
+++ MSBuildLogsExtended/UI/MVVMLight/MSBuildLogsExtended.UWP81/MSBuildLogsExtended.UWP81.Windows/UserControls/UCCommonOfBuildEventCode.xaml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace MSBuildLogsExtended.WinStoreApp.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UCCommonOfBuildEventCode.xaml
+    /// </summary>
+    public partial class UCCommonOfBuildEventCode : UserControl
+    {
+        public UCCommonOfBuildEventCode()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MSBuildLogsExtended/UI/MVVMLight/MSBuildLogsExtended.UWP81/MSBuildLogsExtended.UWP81.Windows/UserControls/UCCommonOfBuildLog.xaml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace MSBuildLogsExtended.WinStoreApp.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UCCommonOfBuildLog.xaml
+    /// </summary>
+    public partial class UCCommonOfBuildLog : UserControl
+    {
+        public UCCommonOfBuildLog()
+        {
+            InitializeComponent();
+        }
+    }
+}
